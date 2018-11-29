244fd83f74cbf9b9644d0918bd29944c20cc116e
Manish Tanwar <manish.tanwar19@gmail.com>
2018-11-29 19:56:46 +0530

    Added fastRead_string

--- Syntax Related/fastio.cpp
+++ Syntax Related/fastio.cpp
@@ -1,4 +1,4 @@
-/*getchar_unlocked and putchar_unlocked doesn't work in windows (Codeforces);replace them with getchar and putchar
+/*getchar_unlocked and putchar_unlocked doesn't work in windows (Codeforces);replace them with getchar and putchar*/
 inline ll read() 
 {
     ll n = 0;
@@ -30,3 +30,24 @@ inline void write(ll a)
         putchar_unlocked(snum[i--]);
     putchar_unlocked('\n');
 }
+
+inline void fastRead_string(char *str)
+{
+    char c = 0;
+    int i = 0;
+
+    while (c < 33)
+        c = getchar_unlocked();
+
+    while (c != '\n') {
+        str[i] = c;
+        c = getchar_unlocked();
+        i = i + 1;
+    }
+    // getchar_unlocked returns -1 on EOF
+    str[i] = '\0';
+}
+// use
+char s[100];
+fastRead_string(s);
+printf("%s\n", s);
