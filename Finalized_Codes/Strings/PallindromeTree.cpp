fdb736fc4553916f0637a5582e73cc12303b3111
smartnj <jainnaman55@gmail.com>
2018-12-08 14:54:56 +0530

    finalized pallindromic tree

--- Finalized_Codes/Strings/PallindromeTree.cpp
+++ Finalized_Codes/Strings/PallindromeTree.cpp
@@ -1,41 +1,34 @@
-
 const ll MAX=1e5+15;
-//ll stpos[MAX];
 ll par[MAX];  // stores index of parent node 
 ll suli[MAX]; // stores index of suffix link
 ll len[MAX];  // stores length of largest pallindrome ending at that node
 ll child[MAX][30];  // stores the children of the node
-
 /*---------------------------------------------------------
 index 0 - root "-1"
 index 1 - root  "0"
 therefore node of s[i] is i+2
 initialize all child[i][j] to -1
 -----------------------------------------------------------*/
-
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);cout<<setprecision(25);
-	ll a,b,c,d,i,j,k,e,f;string s;
-	cin>>s;
-//	stpos[0]=-1;stpos[1]=-1;
+void eer_tree(string s){
+	ll a,b,c,d,i,j,k,e,f;
 	suli[1]=0;suli[0]=0;len[1]=0;len[0]=-1;
 	ll n=s.length();
-	for(i=0;i<n+10;i++)for(j=0;j<30;j++)child[i][j]=-1;
+	for(i=0;i<n+10;i++)
+		for(j=0;j<30;j++)child[i][j]=-1;
 	ll cur=1;d=1;
 	for(i=0;i<s.size();i++){
-		++d;//stpos[d]=i;
+		++d;
 		while(true){
-		//	trace(cur);
 			a=i-1-len[cur];
 			if(a>=0){
 				if(s[a]==s[i]){
-				//	trace(a);
 					if(child[cur][(ll)(s[i]-'a')]==-1){
-						par[d]=cur;child[cur][(ll)(s[i]-'a')]=d;len[d]=len[cur]+2;cur=d;	
-						break;
+						par[d]=cur;child[cur][(ll)(s[i]-'a')]=d;
+						len[d]=len[cur]+2;cur=d;
 					}
 					else{
-						par[d]=cur;len[d]=len[cur]+2;cur=child[cur][(ll)(s[i]-'a')];break;
+						par[d]=cur;len[d]=len[cur]+2;
+						cur=child[cur][(ll)(s[i]-'a')];
 					}
 					break;
 				}
@@ -54,5 +47,4 @@ int main() {
 			suli[d]=child[c][(ll)(s[i]-'a')];
 		}
 	}
-	return 0;
 }
