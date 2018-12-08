f7eaaa28f5cf9780cbf34f761fb4d6e9a1b4b20a
Manish Tanwar <manish.tanwar19@gmail.com>
2018-12-08 13:18:57 +0530

    edited dinic

--- Finalized_Codes/Flows/maxflow_dinics.cpp
+++ Finalized_Codes/Flows/maxflow_dinics.cpp
@@ -1,15 +1,15 @@
-// Time: O(m*n^2) and for any unit capacity network O(m * n^1/2) 
-// TIme: O(min(fm,mn^2)) (f: flow routed)
-// (so for bipartite matching as well)
-// In practice it is pretty fast for any bipartite network
-// I/O:     n -> vertice; DinicFlow net(n);
-//          for(z : edges) net.addEdge(z.F,z.S,cap);
-//          max flow = maxFlow(s,t);
-// e=(u,v), e.flow represents the effective flow from u to v 
-// (i.e f(u->v) - f(v->u)), vertices are 1-indexed 
-struct edge {
-    ll x, y, cap, flow; };
+/*Time: O(m*n^2) and for any unit capacity network O(m * n^1/2) 
+TIme: O(min(fm,mn^2)) (f: flow routed)
+(so for bipartite matching as well)
+In practice it is pretty fast for any bipartite network
+I/O:     n -> vertice; DinicFlow net(n);
+         for(z : edges) net.addEdge(z.F,z.S,cap);
+         max flow = maxFlow(s,t);
+e=(u,v), e.flow represents the effective flow from u to v 
+(i.e f(u->v) - f(v->u)), vertices are 1-indexed
+*** use int if possible(ll could be slow in dinic) *** */
 
+struct edge {ll x, y, cap, flow;};
 struct DinicFlow {
     // *** change inf accordingly *****
     const ll inf = (1e18);
@@ -17,23 +17,19 @@ struct DinicFlow {
     vector <ll> cur, d;
     vector < vector <ll> > adj;
     ll n, source, sink;
-
     DinicFlow() {}
-
     DinicFlow(ll v) {
         n = v;
         cur = vector <ll> (n + 1);
         d = vector <ll> (n + 1);
         adj = vector < vector <ll> > (n + 1);
     }
-
     void addEdge(ll from, ll to, ll cap) {
         edge e1 = {from, to, cap, 0};
         edge e2 = {to, from, 0, 0}; 
         adj[from].push_back(e.size()); e.push_back(e1);
         adj[to].push_back(e.size()); e.push_back(e2);
     }
-
     ll bfs() {
         queue <ll> q;
         for(ll i = 0; i <= n; ++i) d[i] = -1;
@@ -49,7 +45,6 @@ struct DinicFlow {
         }
         return d[sink] >= 0;
     }
-
     ll dfs(ll x, ll flow) {
         if(!flow) return 0;
         if(x == sink) return flow;
@@ -65,7 +60,6 @@ struct DinicFlow {
         }
         return 0;
     }
-
     ll maxFlow(ll src, ll snk) {
         this->source = src; this->sink = snk;
         ll flow = 0;
