11084d8f76f65ac41de902838bf47de1b446c20e
smartnj <jainnaman55@gmail.com>
2018-12-08 14:37:51 +0530

    done some changes

--- Uncommented Codes/PallindromeTree.cpp
+++ Uncommented Codes/PallindromeTree.cpp
@@ -1,31 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-#define TRACE
-#ifdef TRACE
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-	cout << name << " : " << arg1 << std::endl;
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-	const char* comma = strchr(names + 1, ',');cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
-}
-#else
-#define trace(...)
-#endif
-
-
-#define ll long long 
-#define ld long double
-#define pb push_back
-#define F first
-#define S second
-#define I insert
-#define pll pair<ll,ll> 
-#define vll vector<ll> 
-#define vpll vector< pll >
 const ll MAX=1e5+15;
 //ll stpos[MAX];
 ll par[MAX];  // stores index of parent node 
