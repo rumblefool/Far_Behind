a6030ddfe2f9f215fe8faa61e95dc737d14d0910
rumblefool <rayush96@gmail.com>
2018-12-12 23:05:19 +0530

    Added remaining topics

--- Finalized_Codes/Syntax_Related/RANDOM_SHUFFLE.cpp
+++ Finalized_Codes/Syntax_Related/RANDOM_SHUFFLE.cpp
@@ -3,6 +3,8 @@ using namespace std;
 const int N = 3000000;
 int main() {
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> uid(l,r);
+    int x=uid(rng);
 	//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); - for 64 bit unsigned numbers
     vector<int> permutation(N);
     for (int i = 0; i < N; i++)
