bd6b2d2be5315b95acec76977b0ffa1375110d52
rumblefool <rayush96@gmail.com>
2018-12-12 21:52:43 +0530

    Added 2DBIT

---
+++ Finalized_Codes/Data_Structures/2DBIT.cpp
@@ -0,0 +1,27 @@
+//point updates and range sum in a ractangle
+//all indices are 1 indices. to increment value of cell (i,j) by val call update(x,y,val)
+//to find sum of rectangle [a,b]-[c,d] find sum of rectangles [1,1]-[c,d],[1,1]-[c,b],
+//[1,1][a,d] and [1,1]-[a,b] and use inclusion exclusion
+ll bit[MAX][MAX];
+void update(ll x , ll y, ll val)
+{
+	while( x < MAX )
+	{
+		ll y1 = y;
+		while( y1 < MAX )
+			bit[x][y1] += val , y1 += ( y1 & -y1 );
+		x += (x & -x);
+	}
+}
+ll sum(ll x , ll y)
+{
+	ll ans = 0;
+	while( x > 0 )
+	{
+		ll y1 = y;
+		while( y1 > 0 )
+			ans += bit[x][y1] , y1 -= ( y1 & -y1 );
+		x -= (x & -x);
+	}
+	return ans;
+}
