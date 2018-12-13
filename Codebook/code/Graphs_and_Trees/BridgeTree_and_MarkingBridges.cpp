b49de304b4796eeb8244784de00dc77f53ef076a
Manish Tanwar <manish.tanwar19@gmail.com>
2018-12-13 23:30:10 +0530

    final codebook

---
+++ Codebook/code/Graphs_and_Trees/BridgeTree_and_MarkingBridges.cpp
@@ -0,0 +1,38 @@
+vll tree[N],g[N];//edge list rep. of graph
+ll U[M],V[M],vis[N],arr[N],T,dsu[N];
+bool isbridge[M]; // if i'th edge is a bridge edge
+ll adj(ll u,ll e) {
+	return U[e]^V[e]^u;
+}
+ll f(ll x) {
+	return dsu[x]=(dsu[x]==x?x:f(dsu[x]));
+}
+void merge(ll a,ll b) {
+	dsu[f(a)]=f(b);
+}
+ll dfs0(ll u,ll edge) { //mark bridges
+	vis[u]=1;
+	arr[u]=T++;
+	ll dbe = arr[u];
+	for(auto e : g[u]) {
+		ll w = adj(u,e);
+		if(!vis[w])dbe = min(dbe,dfs0(w,e));
+		else if(e!=edge)dbe = min(dbe,arr[w]);
+	}
+	if(dbe==arr[u] && edge!=-1)isbridge[edge]=true;
+	else if(edge!=-1)merge(U[edge],V[edge]);
+	return dbe;
+}
+void buildBridgeTree(ll n,ll m) {
+	for(ll i=1; i<=n; i++)dsu[i]=i;
+	for(ll i=1; i<=n; i++)if(!vis[i])dfs0(i,-1);
+	for(ll i=1; i<=m; i++)
+		if(f(U[i])!=f(V[i])) {
+			tree[f(U[i])].pb(f(V[i]));
+			tree[f(V[i])].pb(f(U[i]));
+		}
+}
+ll n,m;
+for(i=1;i<=m;i++)
+	cin>>U[i]>>V[i]; g[U[i]].pb(i); g[V[i]].pb(i);
+buildBridgeTree(n,m);
