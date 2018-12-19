d41aa8e1d4bb4f4e75078f4137d08d390429760f
smartnj <44122905+smartnj@users.noreply.github.com>
2018-12-19 12:54:10 +0530

    Update template.cpp

--- Codebook/code/Syntax_Related/template.cpp
+++ Codebook/code/Syntax_Related/template.cpp
@@ -1,121 +1,69 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 using namespace std;
+#define ll long long
 
-template<class T> ostream& operator<<(ostream &os, vector<T> V) {
- os << "[ "; for(auto v : V) os << v << " "; return os << "]";}
-template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {
-	return os << "(" << P.first << "," << P.second << ")";}
+ll mxm() {return LLONG_MIN;}
+template<typename... Args>
+ll mxm(ll a, Args... args) { return max(a,mxm(args...)); }
+
+ll mnm() {return LLONG_MAX;}
+template<typename... Args>
+ll mnm(ll a, Args... args) { return min(a,mnm(args...)); }
+
+template<class T> ostream& operator<<(ostream& os,vector<T> V){
+	os<<"[ ";for(auto v:V)os<<v<<" ";return os<<"]";
+}
+template<class L,class R> ostream& operator<<(ostream& os,pair<L,R> P){
+	return os<<"("<<P.first<<","<<P.second<<")";
+}
 
 #define TRACE
 #ifdef TRACE
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-	cout << name << " : " << arg1 << std::endl;
+#define trace(...) __f(#__VA_ARGS__,__VA_ARGS__)
+template<typename Arg1>
+void __f(const char* name,Arg1&& arg1){
+	cout<<name<<" : "<<arg1<<endl;
 }
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-	const char* comma = strchr(names + 1, ',');cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+template <typename Arg1,typename... Args>
+void __f(const char* names,Arg1&& arg1,Args&&... args){
+	const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);
 }
 #else
 #define trace(...) 1
 #endif
 
-#define ll long long
 #define ld long double
-#define vll vector<ll>
 #define pll pair<ll,ll>
+#define ii pair<int,int>
+#define vll vector<ll>
+#define vii vector<ii>
+#define vi vector<int>
 #define vpll vector<pll>
-#define I insert 
-#define pb push_back
+#define vld vector<ld>
+#define vvld vector<vld>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define vvpll vector<vpll>
+#define vvii vector<vii>
+#define I insert
 #define F first
 #define S second
-#define all(x) x.begin(),x.end()
+#define pb push_back
 #define endl "\n"
-// const ll MAX=1e6+5;
+#define all(x)	x.begin(),x.end()
 
 // int mod=1e9+7;
-inline int mul(int a,int b){return (a*1ll*b)%mod;}
 inline int add(int a,int b){a+=b;if(a>=mod)a-=mod;return a;}
 inline int sub(int a,int b){a-=b;if(a<0)a+=mod;return a;}
+inline int mul(int a,int b){return (a*1ll*b)%mod;}
 inline int power(int a,int b){int rt=1;while(b>0){if(b&1)rt=mul(rt,a);a=mul(a,a);b>>=1;}return rt;}
 inline int inv(int a){return power(a,mod-2);}
-inline void modadd(int &a,int b){a+=b;if(a>=mod)a-=mod;} 
-int main(){
+inline void modadd(int &a,int b){a+=b;if(a>=mod)a-=mod;}
+
+int main()
+{
 	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);cout<<setprecision(25);
-	
+			
 }
-
-// clock
-clock_t clk = clock();
-clk = clock() - clk;
-((ld)clk)/CLOCKS_PER_SEC
-
-// fastio
-inline ll read() {
-    ll n = 0; char c = getchar_unlocked();
-    while (!('0' <= c && c <= '9')) c = getchar_unlocked();
-    while ('0' <= c && c <= '9')
-        n = n * 10 + c - '0', c = getchar_unlocked();
-    return n;
-}
-inline void write(ll a){
-    register char c; char snum[20]; ll i=0;
-    do{
-        snum[i++]=a%10+48;
-        a=a/10;
-    }
-    while(a!=0); i--;
-    while(i>=0)
-        putchar_unlocked(snum[i--]);
-    putchar_unlocked('\n');
-}
-using getline, use cin.ignore()
-
-// gp_hash_table
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-gp_hash_table<int, int> table;	//cc_hash_table can also be used
-//custom hash function
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(int x) { return hash<int>{}(x ^ RANDOM); }
-};
-gp_hash_table<int, int, chash> table;
-//custom hash function for pair
-struct chash {
-    int operator()(pair<int,int> x) const { return x.first* 31 + x.second; }
-};
-
-// random
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-uniform_int_distribution<int> uid(l,r);
-int x=uid(rng);
-//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
- // - for 64 bit unsigned numbers
-vector<int> per(N);
-for (int i = 0; i < N; i++)
-    per[i] = i;
-shuffle(per.begin(), per.end(), rng);
-
-// string splitting
-// this splitting is better than custom function(w.r.t time)
-string line = "Ge";
-vector <string> tokens; 
-stringstream check1(line); 
-string ele; 
-// Tokenizing w.r.t. space ' ' 
-while(getline(check1, ele, ' '))
-tokens.push_back(ele);
-//Ordered Sets
-typedef tree<ll,null_type,less<ll>,rb_tree_tag,
-tree_order_statistics_node_update> ordered_set;
-ordered_set X;X.insert(1);X.insert(2);
-*X.find_by_order(0)-> 1
-*X.find_by_order(1)-> 2
-(end(X)==X.find_by_order(2) -> true
-//order_of_key(x) -># of elements < x
-//For multiset use less_equal operator but 
-//it does support erase operations for multiset
