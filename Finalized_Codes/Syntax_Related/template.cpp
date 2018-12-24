ca3e57a60cf124e1baf4baf917b812a41db704b8
smartnj <44122905+smartnj@users.noreply.github.com>
2018-12-24 20:26:55 +0530

    Update template.cpp

--- Finalized_Codes/Syntax_Related/template.cpp
+++ Finalized_Codes/Syntax_Related/template.cpp
@@ -54,7 +54,7 @@ void __f(const char* names,Arg1&& arg1,Args&&... args){
 #define endl "\n"
 #define all(x)	x.begin(),x.end()
 
-// int mod=1e9+7;
+// const int mod=1e9+7;
 inline int add(int a,int b){a+=b;if(a>=mod)a-=mod;return a;}
 inline int sub(int a,int b){a-=b;if(a<0)a+=mod;return a;}
 inline int mul(int a,int b){return (a*1ll*b)%mod;}
