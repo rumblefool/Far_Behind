74fb92d92db1f71c0305b38876d9480ef6de2ce4
smartnj <44122905+smartnj@users.noreply.github.com>
2018-12-25 11:27:25 +0530

    Update template.cpp

--- Codebook/code/Syntax_Related/template.cpp
+++ Codebook/code/Syntax_Related/template.cpp
@@ -7,46 +7,32 @@ using namespace std;
 ll mxm() {return LLONG_MIN;}
 template<typename... Args>
 ll mxm(ll a, Args... args) { return max(a,mxm(args...)); }
-
 ll mnm() {return LLONG_MAX;}
 template<typename... Args>
 ll mnm(ll a, Args... args) { return min(a,mnm(args...)); }
-
 template<class T> ostream& operator<<(ostream& os,vector<T> V){
-	os<<"[ ";for(auto v:V)os<<v<<" ";return os<<"]";
-}
+	os<<"[ ";for(auto v:V)os<<v<<" ";return os<<"]";}
 template<class L,class R> ostream& operator<<(ostream& os,pair<L,R> P){
-	return os<<"("<<P.first<<","<<P.second<<")";
-}
-
+	return os<<"("<<P.first<<","<<P.second<<")";}
 #define TRACE
 #ifdef TRACE
 #define trace(...) __f(#__VA_ARGS__,__VA_ARGS__)
 template<typename Arg1>
 void __f(const char* name,Arg1&& arg1){
-	cout<<name<<" : "<<arg1<<endl;
-}
+	cout<<name<<" : "<<arg1<<endl;}
 template <typename Arg1,typename... Args>
 void __f(const char* names,Arg1&& arg1,Args&&... args){
-	const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);
-}
+	const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);}
 #else
 #define trace(...) 1
 #endif
 
 #define ld long double
 #define pll pair<ll,ll>
-#define ii pair<int,int>
+#define pii pair<int,int>
 #define vll vector<ll>
-#define vii vector<ii>
 #define vi vector<int>
 #define vpll vector<pll>
-#define vld vector<ld>
-#define vvld vector<vld>
-#define vvi vector<vi>
-#define vvll vector<vll>
-#define vvpll vector<vpll>
-#define vvii vector<vii>
 #define I insert
 #define F first
 #define S second
@@ -54,13 +40,12 @@ void __f(const char* names,Arg1&& arg1,Args&&... args){
 #define endl "\n"
 #define all(x)	x.begin(),x.end()
 
-// int mod=1e9+7;
+// const int mod=1e9+7;
 inline int add(int a,int b){a+=b;if(a>=mod)a-=mod;return a;}
 inline int sub(int a,int b){a-=b;if(a<0)a+=mod;return a;}
 inline int mul(int a,int b){return (a*1ll*b)%mod;}
 inline int power(int a,int b){int rt=1;while(b>0){if(b&1)rt=mul(rt,a);a=mul(a,a);b>>=1;}return rt;}
 inline int inv(int a){return power(a,mod-2);}
-inline void modadd(int &a,int b){a+=b;if(a>=mod)a-=mod;}
 
 int main()
 {
