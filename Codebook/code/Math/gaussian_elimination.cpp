45fdbb206dd21f8d092ae60cf56437b1634c0f1a
Manish Tanwar <manish.tanwar19@gmail.com>
2018-12-25 11:53:47 +0530

    Update gaussian_elimination.cpp

--- Codebook/code/Math/gaussian_elimination.cpp
+++ Codebook/code/Math/gaussian_elimination.cpp
@@ -15,24 +15,42 @@
 const double EPS = 1e-10;
 #define vld vector<ld>
 #define vvld vector<vld>
-ld GaussJordan(vvld &a, vvld &b) {
+
+ll GaussJordan(vvll &a, vvll &b) {
 	const int n = a.size(), m = b[0].size();
-	vll irow(n),icol(n),ipiv(n,0);ld det = 1;
-	for (int i=0;i<n;i++) {
-		int pj=-1,pk=-1;for(int j=0;j<n;j++) 
-		if(!ipiv[j])for(int k=0;k<n;k++)
-		if(!ipiv[k])if(pj==-1||fabs(a[j][k])>fabs(a[pj][pk])){pj=j;pk=k;}
-	if(fabs(a[pj][pk])<EPS){return 0;}
-	ipiv[pk]++;swap(a[pj],a[pk]);swap(b[pj],b[pk]);
-	if(pj!=pk)det*=-1;irow[i]=pj;icol[i]=pk;
-	ld c=1.0/a[pk][pk];det*=a[pk][pk];a[pk][pk]=1;
-	for(int p=0;p<n;p++) a[pk][p]*=c;
-	for(int p=0;p<m;p++)b[pk][p]*=c;
-	for(int p=0;p<n;p++) if(p!=pk){c=a[p][pk];
-	a[p][pk]=0;
-	for(int q=0;q<n;q++) a[p][q]-=a[pk][q]*c;
-	for(int q=0;q<m;q++)b[p][q]-=b[pk][q]*c;}}
-	for (int p=n-1;p>=0;p--) if(irow[p]!=icol[p]){
-	for(int k=0;k<n;k++)
-		swap(a[k][irow[p]],a[k][icol[p]]);}
-	return det;}
+	int n1 = a[0].size();
+	vll irow(n),icol(n),ipiv(n,0);ll det = 1;
+	for (int i=0;i<n1;i++) {
+		int pj=-1,pk=-1;
+		for(int j=0;j<n;j++) 
+			if(!ipiv[j])
+				for(int k=0;k<n1;k++)
+					if(!ipiv[k])
+						if(pj==-1||fabs(a[j][k])>fabs(a[pj][pk])){pj=j;pk=k;}
+		// if(fabs(a[pj][pk])<EPS){return 0;}  // uncomment in double version
+		ipiv[pk]++;
+		swap(a[pj],a[pk]);swap(b[pj],b[pk]);
+		if(a[pk][pk]==0)return 0;  // comment in double version
+		if(pj!=pk)det = mul(det,mod-1);   // det*=-1;
+		irow[i]=pj;icol[i]=pk;
+		ll c=inv(a[pk][pk]);det = mul(det,a[pk][pk]);
+		a[pk][pk]=1;
+		for(int p=0;p<n1;p++) a[pk][p] = mul(a[pk][p], c);
+		for(int p=0;p<m;p++) b[pk][p] = mul(b[pk][p], c);
+		for(int p=0;p<n;p++){ 
+			if(p!=pk){
+				c=a[p][pk];
+				a[p][pk]=0;
+				for(int q=0;q<n1;q++) a[p][q] = sub(a[p][q], mul(a[pk][q],c));
+				for(int q=0;q<m;q++) b[p][q] = sub(b[p][q], mul(b[pk][q],c));
+			}
+		}
+	}
+	// comment below if not square matrix . 
+	for (int p=n-1;p>=0;p--)
+		if(irow[p]!=icol[p]){
+			for(int k=0;k<n;k++)
+				swap(a[k][irow[p]],a[k][icol[p]]); 
+		} 
+	return det;
+}
