077223c202cb928bfcbe126270f7a3a2a060def4
Manish Tanwar <manish.tanwar19@gmail.com>
2018-12-25 11:53:11 +0530

    Added clock,ordered_set

--- Codebook/code/Syntax_Related/template.cpp
+++ Codebook/code/Syntax_Related/template.cpp
@@ -53,3 +53,74 @@ int main()
 	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);cout<<setprecision(25);
 			
 }
+// clock
+clock_t clk = clock();
+clk = clock() - clk;
+((ld)clk)/CLOCKS_PER_SEC
+
+// fastio
+inline ll read() {
+    ll n = 0; char c = getchar_unlocked();
+    while (!('0' <= c && c <= '9')) c = getchar_unlocked();
+    while ('0' <= c && c <= '9')
+        n = n * 10 + c - '0', c = getchar_unlocked();
+    return n;
+}
+inline void write(ll a){
+    register char c; char snum[20]; ll i=0;
+    do{
+        snum[i++]=a%10+48;
+        a=a/10;
+    }
+    while(a!=0); i--;
+    while(i>=0)
+        putchar_unlocked(snum[i--]);
+    putchar_unlocked('\n');
+}
+// gp_hash_table
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+gp_hash_table<int, int> table;  //cc_hash_table can also be used
+//custom hash function
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) { return hash<int>{}(x ^ RANDOM); }
+};
+gp_hash_table<int, int, chash> table;
+//custom hash function for pair
+struct chash {
+    int operator()(pair<int,int> x) const { return x.first* 31 + x.second; }
+};
+
+// random
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> uid(l,r);
+int x=uid(rng);
+//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ // - for 64 bit unsigned numbers
+vector<int> per(N);
+for (int i = 0; i < N; i++)
+    per[i] = i;
+shuffle(per.begin(), per.end(), rng);
+
+// string splitting
+// this splitting is better than custom function(w.r.t time)
+using getline, use cin.ignore()
+
+string line = "Ge";
+vector <string> tokens; 
+stringstream check1(line); 
+string ele; 
+// Tokenizing w.r.t. space ' ' 
+while(getline(check1, ele, ' '))
+tokens.push_back(ele);
+//Ordered Sets
+typedef tree<ll,null_type,less<ll>,rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+ordered_set X;X.insert(1);X.insert(2);
+*X.find_by_order(0)-> 1
+*X.find_by_order(1)-> 2
+(end(X)==X.find_by_order(2) -> true
+//order_of_key(x) -> # of elements < x
+//For multiset use less_equal operator but 
+//it does support erase operations for multiset 
