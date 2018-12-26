481b358f56690000ad75f1c50c28f15ae7ac3c49
rumblefool <rayush96@gmail.com>
2018-12-26 16:41:15 +0530

    Shortened centroid decomposition

--- Codebook/code/Graphs_and_Trees/Centroid_Decomp_Ayush.cpp
+++ Codebook/code/Graphs_and_Trees/Centroid_Decomp_Ayush.cpp
@@ -3,14 +3,12 @@ dist[nx]: dist[i][0][j]=no. of nodes at dist k in subtree of i in centroid tree
 dist[i][j][k]=no. of nodes at distance k in jth child of i in centroid tree ***(use adj while doing dfs instead of adj1)***
 preprocess: stores all values in dist array*/
 const int nx=1e5;
-vector<int> adj[nx],adj1[nx];
-int par[nx],timstmp[nx],ssize[nx],vis[nx];
-int tim=1;
-vector<int> cntrorder;//centroids in order
-vector<vector<int> > dist[nx];
+vi adj[nx],adj1[nx];
+int par[nx],timstmp[nx],ssize[nx],vis[nx],tim=1;
+vi corder;//centroids in order
+vvi dist[nx];
 int dfs(int root){
-	vis[root]=tim;
-	int t=0;
+	vis[root]=tim;int t=0;
 	for(auto i:adj[root]){
 		if(!timstmp[i]&&vis[i]<tim)t+=dfs(i);}
 	ssize[root]=t+1;return t+1;}
@@ -22,37 +20,28 @@ int dfs1(int root,int n){
 			poss&=(ssize[i]<=n/2),mxc=max(mxc,{ssize[i],i});}
 	if(poss&&(n-ssize[root])<=n/2)return root;
 	return dfs1(mxc.second,n);}
-int findc(int root){
-	dfs(root);
-	int n=ssize[root];tim++;
-	return dfs1(root,n);}
+int findc(int root){dfs(root);
+	int n=ssize[root];tim++;return dfs1(root,n);}
 void cntrdecom(int root,int p){
-	int cntr=findc(root);
-	cntrorder.pb(cntr);
+	int cntr=findc(root);corder.pb(cntr);
 	timstmp[cntr]=tim++;par[cntr]=p;
 	if(p>=0)adj1[p].pb(cntr);
-	for(auto i:adj[cntr])
-		if(!timstmp[i])
+	for(auto i:adj[cntr])if(!timstmp[i])
 			cntrdecom(i,cntr);}
 void dfs2(int root,int nod,int j,int dst){
-	if(dist[root][j].size()==dst)dist[root][j].pb(0);
+	if(dist[root][j].size()==dst)
+		dist[root][j].pb(0);
 	vis[nod]=tim;dist[root][j][dst]+=1;
 	for(auto i:adj[nod]){
 		if((timstmp[i]<=timstmp[root])||(vis[i]==vis[nod]))continue;
-		vis[i]=tim;dfs2(root,i,j,dst+1);}
-}
+		vis[i]=tim;dfs2(root,i,j,dst+1);}}
 void preprocess(){
-	for(int i=0;i<cntrorder.size();i++){
-		int root=cntrorder[i];
-		vector<int> temp;
+	for(int i=0;i<corder.size();i++){
+		int root=corder[i];vi temp;
 		dist[root].pb(temp);temp.pb(0);++tim;
-		dfs2(root,root,0,0);
-		int cnt=0;
+		dfs2(root,root,0,0);int cnt=0;
 		for(int j=0;j<adj[root].size();j++){
 			int nod=adj[root][j];
-			if(timstmp[nod]<timstmp[root])
-				continue;
+			if(timstmp[nod]<timstmp[root])continue;
 			dist[root].pb(temp);++tim;
-			dfs2(root,nod,++cnt,1);}
-	}
-}
+			dfs2(root,nod,++cnt,1);}}}
