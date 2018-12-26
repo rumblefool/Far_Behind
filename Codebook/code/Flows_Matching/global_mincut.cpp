da0419728c104a9272ba5de3d86ee834aa2f9821
Manish Tanwar <manish.tanwar19@gmail.com>
2018-12-26 16:42:03 +0530

    modified codebook

--- Codebook/code/Flows_Matching/global_mincut.cpp
+++ Codebook/code/Flows_Matching/global_mincut.cpp
@@ -1,14 +1,12 @@
 /*finds min weighted cut in undirected graph in
 O(n^3), Adj Matrix, 0-indexed vertices
 output-(min cut value, nodes in half of min cut)*/
-typedef vector<int> VI;
-typedef vector<VI> VVI;
-const int INF = 1000000000;
+typedef vector<int> VI;typedef vector<VI> VVI;
+const int INF = 1e9;
 pair<int, VI> GetMinCut(VVI &weights) {
 	int N = weights.size();
 	VI used(N), cut, best_cut;
 	int best_weight = -1;
-	
 	for (int phase = N-1; phase >= 0; phase--) {
 		VI w = weights[0];
 		VI added = used;
@@ -16,15 +14,15 @@ pair<int, VI> GetMinCut(VVI &weights) {
 		for (int i = 0; i < phase; i++) {
 			prev = last; last = -1;
 			for (int j = 1; j < N; j++)
-			if(!added[j] && (last == -1 || w[j] > w[last])) 
-					last = j;
+			if(!added[j] && (last==-1 || w[j]>w[last])) 
+				last = j;
 			if (i == phase-1) {
 				for(int j=0;j<N;j++) 
 					weights[prev][j] += weights[last][j];
 				for(int j=0;j<N;j++) 
 					weights[j][prev] = weights[prev][j];
 				used[last] = true; cut.push_back(last);
-				if (best_weight==-1 || w[last]<best_weight)
+				if(best_weight==-1 || w[last]<best_weight)
 					best_cut = cut, best_weight = w[last];
 			}
 			else {
