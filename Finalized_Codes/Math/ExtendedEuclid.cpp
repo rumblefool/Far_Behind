3695e4c1fbcd12afed91509a8bef2e8cd430aa8c
rumblefool <39515218+rumblefool@users.noreply.github.com>
2020-05-02 02:57:09 +0530

    Added modulo inverse

--- Finalized_Codes/Math/ExtendedEuclid.cpp
+++ Finalized_Codes/Math/ExtendedEuclid.cpp
@@ -1,4 +1,11 @@
-int gcd(int a,int b,int &x,int &y) {
+int euclid(int a,int b,int &x,int &y) {
 	if (a == 0) {x = 0; y = 1;return b;}
-	int x1,y1,d = gcd(b%a, a, x1, y1);
-	x = y1 - (b / a) * x1;y = x1;return d;}
+	int x1,y1,d = euclid(b%a, a, x1, y1);
+	x = y1 - (b / a) * x1;y = x1;return d;
+}
+
+int modinv(int a, int b){
+	int x, y;
+	euclid(a, b, x, y);
+	return (x + b) % b;
+}
